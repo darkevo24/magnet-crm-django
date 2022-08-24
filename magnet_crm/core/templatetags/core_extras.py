8a90c50b01be1ba0520d4abcfcd71aae96ec00df
Aristo <ivan_satrija@live.com>
2022-08-24 18:53:08 +0700

    updates

--- magnet_crm/core/templatetags/core_extras.py
+++ magnet_crm/core/templatetags/core_extras.py
@@ -2,6 +2,7 @@ from django import template
 from django.template.defaultfilters import stringfilter
 from django.conf import settings
 import urllib.parse
+from staff.models import *
 
 register = template.Library()
 
@@ -10,6 +11,12 @@ register = template.Library()
 @register.filter(name='add_class')
 def add_class(field, css):
    return field.as_widget(attrs={"class":css})
+
+
+@register.filter(name='full_menu')
+def full_menu(user):
+   staff_level = Staff.objects.filter(profile__user=user).first().staff_level.level
+   return staff_level < 3
 
 
 @register.filter(name='dict_get')
