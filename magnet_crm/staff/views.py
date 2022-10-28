924fcc279e70afb859a9e241d419a15ce1ea4780
ivan satrija <ivan_satrija@yahoo.com>
2022-10-28 13:10:11 +0700

    up

--- magnet_crm/staff/views.py
+++ magnet_crm/staff/views.py
@@ -569,10 +569,31 @@ def staff_report_detail(request,staff_uid):
 		all_client += x.client.magnet_id + ","
 	all_so = get_so_list(all_client[:-1])
 
-	
+	total_client = 0
+	total_client = len(all_so['data'])
+
+	amount = 0 
+	for y in all_so['data']:
+		amount += int(float(y['ftd']))
+
+	print(total_client,"total_client")
+	bonus_per_ft = 0
+	for y in all_so['data']:
+		if total_client > 15 and amount > 15000:
+			bonus_per_ft = 35
+		elif total_client >= 10 and total_client <= 14 and amount > 5000:
+			bonus_per_ft = 15
+		elif total_client >= 5 and total_client <= 9 and amount > 2500:
+			bonus_per_ft = 10
+
+
 	context = {
 		'staff_list': staff_list,
 		'menu':'staff_list',
 		'all_so':all_so['data'],
+		'bonus_per_ft': bonus_per_ft,
+		'total_amount' : amount,
+		'total_client' :  total_client,
+
 	}
 	return render(request,template,context=context)
