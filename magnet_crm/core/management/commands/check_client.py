64e37ee3ea43c1eeee0b7980f422701247233c4c
Ivan <ivan_satrija@live.com>
2023-01-17 16:28:03 +0700

    updated

--- magnet_crm/core/management/commands/check_client.py
+++ magnet_crm/core/management/commands/check_client.py
@@ -26,7 +26,7 @@ class Command(BaseCommand):
 		# parser.add_argument('username', type=str, help='username & password')
 
 	def handle(self, *args, **kwargs):
-		
+		now = timezone.now()
 		# cek_user.username = cek_user.email
 		# cek_user.save()
 		# print('-----')
@@ -37,8 +37,21 @@ class Command(BaseCommand):
 			with transaction.atomic():
 
 				clients = Client.objects.filter(nama__icontains='Dhensu')
+				clients = Client.objects.filter(is_active=True)
+				print(clients.count())
+				clients = Client.objects.filter(is_active=True).exclude(magnet_id='').exclude(magnet_id=None)
+				print(clients.count())
+				client_magnet_ids = []
 				for client in clients:
-					print(client.nama, client.magnet_id)
+					client_magnet_ids.append(client.magnet_id)
+
+				print(client_magnet_ids)
+
+				
+
+
+				# for client in clients:
+				# 	print(client.nama, client.magnet_id)
 
 				cnx = mysql.connector.connect(
 						host="54.151.138.128",
@@ -51,15 +64,39 @@ class Command(BaseCommand):
 				mycursor.execute("SHOW COLUMNS FROM v_users;")
 				myresult = mycursor.fetchall()
 				client_email = str(client.email)
-				print('show able', mycursor)
+				print('show able', myresult)
 				
-				client_id = '152876'
-				str_sql = "SELECT * FROM v_users WHERE  id = '" +  client_id + "';";
+				client_id = '152859'
+				str_sql = "SELECT * FROM v_users WHERE id IN (""" + str(client_magnet_ids)[:-1][1:]+ ");"
+
 				
 				mycursor.execute(str_sql)
-				result = mycursor.fetchone()
-				print(result)
+				results = mycursor.fetchall()
+				client_id_name_dict = {}
+				client_id_phone_dict = {}
+				client_id_email_dict = {}
+				for result in results:
+					client_id_name_dict[result[0]] = result[1]
+					client_id_phone_dict[result[0]] = result[10]
+					client_id_email_dict[result[0]] = result[5]
+				# print(result)
+				# print(client_id_name_dict)
+				# print(client_id_phone_dict)
+				# print(client_id_email_dict)
 				cnx.close()
+
+				
+				for client in clients:
+					magnet_id = int(client.magnet_id)
+					client_email = client_id_email_dict[magnet_id]
+					client_name = client_id_name_dict[magnet_id]
+					client_phone_number = client_id_phone_dict[magnet_id]
+					print(client_email, client_name, client_phone_number)
+					client.nama = client_name 
+					client.phone_no = client_phone_number
+					client.updated_at = now
+					client.save()
+
 
 				 
 
